9de6b2465b14bc236afa69be91135776b673043d
buitanphat247 <buitanphat2747@thpt-vungtau.edu.vn>
2024-07-17 08:11:54 +0700

    update tsconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from '@/redux/store';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "@/redux/store";
+import App from "./app"; // Đảm bảo đường dẫn và tên tệp đúng
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
